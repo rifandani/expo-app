7b4341a8d0169e8f60b0778e89a0d43bcc386ab1
rifandani <ipandani2505@gmail.com>
2024-01-25 21:47:47 +0700

    feat: add react-query and navigation devtools plugin

--- src/bootstrap/providers/navigation/provider.tsx
+++ src/bootstrap/providers/navigation/provider.tsx
@@ -1,4 +1,6 @@
+import { useReactNavigationDevTools } from '@dev-plugins/react-navigation';
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import { useNavigationContainerRef } from 'expo-router';
 import { PropsWithChildren, useMemo } from 'react';
 import { useColorScheme } from 'react-native';
 
@@ -6,8 +8,10 @@ import { useAppStore } from '#shared/hooks/use-app-store';
 
 /**
  * Wether to render `DarkTheme` or `DefaultTheme` based on the user color scheme.
+ * Also includes react navigation devtools in development.
  */
-export function AppNavigationThemeProvider({ children }: PropsWithChildren) {
+export function AppNavigationProvider({ children }: PropsWithChildren) {
+  const navigationRef = useNavigationContainerRef();
   const scheme = useColorScheme();
   const theme = useAppStore((state) => state.theme);
   const value = useMemo(
@@ -21,6 +25,8 @@ export function AppNavigationThemeProvider({ children }: PropsWithChildren) {
           : DefaultTheme,
     [scheme, theme]
   );
+
+  useReactNavigationDevTools(navigationRef);
 
   return <ThemeProvider value={value}>{children}</ThemeProvider>;
 }
--- src/bootstrap/providers/query/provider.tsx
+++ src/bootstrap/providers/query/provider.tsx
@@ -1,8 +1,11 @@
+import { useReactQueryDevTools } from '@dev-plugins/react-query';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { PropsWithChildren } from 'react';
 
 import { queryClient } from '#bootstrap/providers/query/query-client';
 
 export function AppQueryProvider({ children }: PropsWithChildren) {
+  useReactQueryDevTools(queryClient);
+
   return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
 }
